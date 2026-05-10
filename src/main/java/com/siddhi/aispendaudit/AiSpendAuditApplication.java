fdd191133b98b1588036f9924e2fc034bc9a2f59
Siddhi Rastogi <vikassiddhi48@gmail.com>
2026-05-11 00:40:41 +0530

    chore: initial project sync and architecture setup

---
+++ src/main/java/com/siddhi/aispendaudit/AiSpendAuditApplication.java
@@ -0,0 +1,13 @@
+package com.siddhi.aispendaudit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSpendAuditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiSpendAuditApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/siddhi/aispendaudit/AiSpendAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddhi.aispendaudit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSpendAuditApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
